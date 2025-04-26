3f640c3c8b51f1f9b683cbcaed2fcc6907e13601
Ubuntu <Jon@Jhh.net>
2025-04-26 18:40:01 +0800

    Simulate local have a good implement.java

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,15 +1,18 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author Arvid Jia 
+ *  @author YOUR NAME HERE
  */
 public class Collatz {
+
+    /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n % 2 == 0) {
-            return n / 2;
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
+            return 3 * n + 1;
         } else {
-            return n * 3 + 1;
+            return n * 2;
         }
     }
-
 
     public static void main(String[] args) {
         int n = 5;
